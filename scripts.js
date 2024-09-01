30b8190fc9978c5c783f2e3d584207c71726a98b
Reeba-code <70468182+Reeba-code@users.noreply.github.com>
2024-09-01 23:00:32 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
-// Loader Logic
+
 window.addEventListener('load', function() {
     setTimeout(function() {
         document.getElementById('loader').style.display = 'none';
         document.getElementById('content').classList.remove('hidden');
-    }, 3000); // Loader duration
+    }, 3000);
 });
 
-// Subscription Form Logic
+
 document.getElementById('subscription-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Thank you for subscribing!');
